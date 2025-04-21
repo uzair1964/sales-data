eebf2423b0545d722f246164929e6ef23020cadf
uzair <uzairkhan19643@gmail.com>
2025-04-21 17:13:35 +0530

    Add files via upload

---
+++ Build_week.sql
@@ -0,0 +1,42 @@
+USE Retail_Sale;
+
+CREATE TABLE sales(Transaction_ID INT, Date_ DATE, Customer_ID VARCHAR(20), Gender VARCHAR(20), Age INT, Product_Category VARCHAR(20), Quantity INT, Price_per_Unit INT, Total_Amount INT, TotalPrice INT);
+
+-- 1. Top 10 customers by total spending.
+SELECT
+	*
+FROM
+	Sales
+Order BY
+	TotalPrice DESC
+LIMIT 10;
+
+-- 2. Total sales by Product category.
+Select
+	Product_Category,
+    SUM(TotalPrice)
+FROM
+	Sales
+GROUP BY
+	Product_Category;
+
+-- 3. Find the month with the highest sales.
+SELECT
+	MONTH(Date_),
+    SUM(TotalPrice)
+FROM
+	Sales
+GROUP BY
+	MONTH(Date_)
+ORDER BY
+	SUM(TotalPrice) DESC
+LIMIT 1;
+
+-- 4. Average order value per customer.
+SELECT
+	Customer_ID,
+    AVG(TotalPrice)
+FROM
+	Sales
+GROUP BY
+	Customer_ID;
